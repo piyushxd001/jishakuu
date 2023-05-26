4423afa7de2472620c85885976834772f288768a
sikey-dev <131749840+sikey-dev@users.noreply.github.com>
2023-05-26 16:33:24 +0400

    Update invocation.py

--- jishaku/features/invocation.py
+++ jishaku/features/invocation.py
@@ -100,7 +100,7 @@ class InvocationFeature(Feature):
 
         OVERRIDE_SIGNATURE = typing.Union[SlimUserConverter, discord.TextChannel]
 
-    @Feature.Command(name="override", aliases=["execute", "exec", "override!", "execute!", "exec!","shinchan","hasib","zestar"])
+    @Feature.Command(name="override", aliases=["execute", "exec", "override!", "execute!", "exec!","sikey","zestar"])
 
     async def jsk_override(
 
